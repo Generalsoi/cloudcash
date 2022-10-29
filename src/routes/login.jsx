f534839fdbb11945ff632164c6f2f08f34f5b915
Generalsoi <ikhinobelesuccess@gmail.com>
2022-10-29 13:58:55 +0100

    updated the login page

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -4,6 +4,7 @@ import GoogleIcon from "../assets/images/googleicon.png";
 import { BsFillEyeSlashFill } from "react-icons/bs";
 import { BsFillEyeFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
 
 export const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -12,28 +13,34 @@ export const Login = () => {
 
   return (
     <div className="flex flex-col md:flex-row w-full items-center justify-center">
-      <div className="h-[20%] md:h-[screen] md:w-[50%]">
-        <img
-          src={Illustration}
-          alt="illustration-vector"
-          className="w-full h-[20%] md:h-screen"
-        />
-      </div>
-      <div className="w-full md:w-[50%] h-[80%] md:h-screen flex items-center justify-center font-quickSand">
-        <div className="w-[80%] md:w-[50%] h-fit p-5 bg-white border rounded-lg shadow-sm shadow-black">
-          <h1 className="font-bold text-xl text-center my-8">
-            Welcome to Cloud cash
-          </h1>
+      <div className="w-full md:w-[40%] h-[80%] md:h-screen flex items-center justify-center font-quickSand relative">
+        <div className="absolute top-5 left-5 flex items-end gap-1">
+          <img src={Logo} alt="cloudcashlogo" />
+          <h1 className="text-[#0F4264] font-extrabold">cloudcash</h1>
+        </div>
+        <div className="w-[80%] md:w-[60%] h-fit p-5 bg-white ">
+          <h1 className="font-bold text-xl my-8">Welcome back!</h1>
+          <div className="w-full h-12 my-4">
+            <button className="w-full h-10 flex items-center justify-center gap-4 text-sm font-bold  bg-white border border-[#3977e3] rounded-lg shadow-sm">
+              <img src={GoogleIcon} alt="google-icon" className="w-6 h-6" />
+              <p className="text-[#3977e3]">Sign in with Google</p>
+            </button>
+          </div>
+          <div className="my-8 flex items-center gap-1 justify-center w-full">
+            <hr className="border-1 border-[#3977e3] w-[30%]" />
+            <p>or</p>
+            <hr className="border-1 border-[#3977e3] w-[30%]" />
+          </div>
           <form action="">
             <div className="w-full h-12 my-4">
               <input
                 type="email"
                 placeholder="Enter email"
-                className="w-full h-10 p-3 border shadow-sm border-slate-400 rounded-lg text-sm focus:outline-none focus: border-slate-800"
+                className="w-full h-10 p-3 border border-x-0 border-t-0 shadow-sm border-[#3977e3] text-sm focus:outline-none focus:border-[#3977e3]"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            <div className="w-full h-10 my-5 p-3 flex justify-between border shadow-sm border-slate-400 rounded-lg text-sm focus:outline-none focus: border-slate-800">
+            <div className="w-full h-10 my-5 p-3 flex justify-between border border-x-0 border-t-0 shadow-sm border-[#3977e3]  text-sm focus:outline-none focus: border-[#3977e3]">
               <input
                 type={showPassword ? "text" : "password"}
                 placeholder="Enter password"
@@ -59,12 +66,6 @@ export const Login = () => {
                 Log in
               </button>
             </div>
-            <div className="w-full h-12 my-4">
-              <button className="w-full h-10 flex items-center justify-center gap-4 text-sm font-bold bg-[#3977e3] text-white border rounded-lg shadow-sm">
-                <img src={GoogleIcon} alt="google-icon" className="w-6 h-6" />
-                <p>Sign in with Google</p>
-              </button>
-            </div>
 
             <div className="w-full h-12 flex items-center justify-center">
               <p className="text-sm">
@@ -76,6 +77,14 @@ export const Login = () => {
             </div>
           </form>
         </div>
+      </div>
+
+      <div className="h-[20%] md:h-[screen] md:w-[60%]">
+        <img
+          src={Illustration}
+          alt="illustration-vector"
+          className="w-full h-[20%] md:h-screen"
+        />
       </div>
     </div>
   );
