d041d6fc15e4db06a31c2149a5e35ec25f5e4c14
Generalsoi <ikhinobelesuccess@gmail.com>
2022-11-22 11:29:45 +0100

    updates

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -30,32 +30,41 @@ export const Content = () => {
 
       <section className="p-6 flex items-start justify-between">
         <div className="md:w-[48%]">
-          <div className="w-full h-fit p-4 bg-white border rounded-2xl ">
-            <h3>Cards</h3>
-            <div className="flex items-center">
+          <div
+            className="w-full h-fit p-6 bg-white border rounded-2xl "
+            style={{
+              boxShadow:
+                "0px 16px 24px rgba(0, 0, 0, 0.06), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 0px 1px rgba(0, 0, 0, 0.04)",
+            }}
+          >
+            <h3 className="font-quickSand text-lg font-bold text-[#404040]">
+              Cards
+            </h3>
+            <div className="flex items-center justify-between font-quickSand">
               <img src={Creditcard} alt="user-credit-card" />
-              <div>
-                <span className="flex flex-col">
-                  <h2>$2850.34</h2>
-                  <p>Current balance</p>
+              <div className="text-end">
+                <span className="flex flex-col mb-4">
+                  <h2 className="text-3xl text-[#197BBD] font-bold">
+                    $2850.34
+                  </h2>
+                  <p className="text-xs text-[#AEAEAE]">Current balance</p>
+                </span>
+                <span className="flex flex-col mb-4">
+                  <h4 className="text-xl text-[#439A86] font-bold">$1600.00</h4>
+                  <p className="text-xs text-[#AEAEAE]">Income</p>
                 </span>
                 <span className="flex flex-col">
-                  <h4>$1600.00</h4>
-                  <p>Income</p>
-                </span>
-                <span className="flex flex-col">
-                  <h5>$1250.34</h5>
-                  <p>Outcome</p>
+                  <h5 className="text-lg text-[#BB4430] font-bold">$1250.34</h5>
+                  <p className="text-xs text-[#AEAEAE]">Outcome</p>
                 </span>
               </div>
-
-              <div>
-                <span>
-                  <p>Weekly payment limit</p>
-                  <span>$350/$4000</span>
-                </span>
-                <span>Deactivate card</span>
-              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <span className="w-[60%] flex items-center text-sm justify-between">
+                <p className="text-[#AEAEAE]">Weekly payment limit</p>
+                <span className="text-[#404040] font-bold">$350/$4000</span>
+              </span>
+              <span className="text-[#AEAEAE]">Deactivate card</span>
             </div>
           </div>
           <div></div>
