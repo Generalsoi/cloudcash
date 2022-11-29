86010a6303a58e468c7cf6f2698beb8116a58d99
Generalsoi <ikhinobelesuccess@gmail.com>
2022-11-29 11:09:39 +0100

    fix: add the outcome section of the dashboard content

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AiOutlineMail, AiOutlineShoppingCart } from "react-icons/ai";
 import { BsPersonCircle, BsTruck, BsPerson } from "react-icons/bs";
 import { BiRestaurant } from "react-icons/bi";
+import { GiCommercialAirplane } from "react-icons/gi";
 import { UserAuth } from "../context/AuthContext";
 import Creditcard from "../assets/images/creditcard.png";
 import Holiday from "../assets/images/holidays.png";
@@ -211,6 +212,46 @@ export const Content = () => {
                   <p>x box</p>
                 </span>
               </span>
+            </div>
+          </div>
+          <div className="mt-8">
+            <h2 className="font-bold text-lg mb-5">Outcome Statistics</h2>
+
+            <div className="flex items-start justify-between mb-5 w-full">
+              <span className="h-8 w-8 border border-none rounded-lg bg-[#FFEADA] flex items-center justify-center">
+                <AiOutlineShoppingCart className="text-[#F8964C]" />
+              </span>
+              <div className="w-[80%] flex flex-col justify-start gap-2">
+                <span className="relative h-2 w-full border border-none rounded-lg bg-[#AEAEAE]">
+                  <span className="absolute left-0 top-0 h-2 w-[51%] bg-[#F8964C] border border-none rounded-lg"></span>
+                </span>
+                <p className="text-[#AEAEAE] text-xs">Shopping</p>
+              </div>
+              <p className="text-[#696969] font-bold text-xl">51%</p>
+            </div>
+            <div className="flex items-start justify-between mb-5 w-full">
+              <span className="h-8 w-8 border border-none rounded-lg bg-[#DDF9E4] flex items-center justify-center">
+                <BsTruck className="text-[#22A447]" />
+              </span>
+              <div className="w-[80%] flex flex-col justify-start gap-2">
+                <span className="relative h-2 w-full border border-none rounded-lg bg-[#AEAEAE]">
+                  <span className="absolute left-0 top-0 h-2 w-[25%] bg-[#22A447] border border-none rounded-lg"></span>
+                </span>
+                <p className="text-[#AEAEAE] text-xs">Electronics</p>
+              </div>
+              <p className="text-[#696969] font-bold text-xl">25%</p>
+            </div>
+            <div className="flex items-start justify-between mb-5 w-full">
+              <span className="h-8 w-8 border border-none rounded-lg bg-[#E4F0FF] flex items-center justify-center">
+                <GiCommercialAirplane className="text-[#70A6E8]" />
+              </span>
+              <div className="w-[80%] flex flex-col justify-start gap-2">
+                <span className="relative h-2 w-full border border-none rounded-lg bg-[#AEAEAE]">
+                  <span className="absolute left-0 top-0 h-2 w-[79%] bg-[#70A6E8] border border-none rounded-lg"></span>
+                </span>
+                <p className="text-[#AEAEAE] text-xs">Travel</p>
+              </div>
+              <p className="text-[#696969] font-bold text-xl">79%</p>
             </div>
           </div>
         </div>
